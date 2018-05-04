d720079701fba4ded6840471f3ad4735eebe13bd
cmardom <cmardonespino@gmail.com>
2018-05-04 11:50:48 -0300

    Se agrega archivo con variables de entorno

---
+++ Github/variables.py
@@ -0,0 +1,4 @@
+# -- coding: utf-8 --
+
+NAME_ORGANIZATION = 'ogranizacionPrueba123'
+PRIVATE_TOKEN_REPO = '0b2775e36cc81c9fce44784ab09d2956d7b2a264'
---
+++ Gitlab/variables.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+
+URL_GIT_REPO = 'http://gitlab.bci.cl'
+PRIVATE_TOKEN_REPO = 'twxCBBbpVkZuhMS9Dc3B'
+API_VERSION = 3
