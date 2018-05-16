7daadb0814d8a1d941dde1f13a1154d5b366398d
Carlos Mardones Pino <excmapi@dom_bci_prod.cl>
2018-05-16 11:16:17 -0300

    Se modifica archivo

--- Github/variables.py
+++ Github/variables.py
@@ -1,14 +1,15 @@
 # -- coding: utf-8 --
 
 REPOS_TO_CLONE = [
-	'repoPrueba1',
-	'repoPrueba5'
+	'repoPrueba1'
 ]
+
 
 ################################################################################################
 #Con estas variables se tiene acceso al nombre de todos los repositorios privados de un usuario#
 ################################################################################################
 
+#NAME_ORGANIZATION = 'BancodeCreditoeInversiones'
 NAME_ORGANIZATION = 'ogranizacionPrueba123'
 PRIVATE_TOKEN_REPO = ''
 USER_NAME = ''
