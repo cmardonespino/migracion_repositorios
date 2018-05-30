b53ef783027f6f0c130c456114a2695bc5723391
cmardonespino <cmardonespino@gmail.com>
2018-05-30 17:53:14 -0400

    Se modifica codigo

--- Github/variables.py
+++ Github/variables.py
@@ -1,8 +1,7 @@
 # -- coding: utf-8 --
 
 REPOS_TO_CLONE = [
-	'repoPrueba5',
-	'ogranizacionPrueba123/repoPrueba2'
+	'repoPrueba5'
 ]
 
 ################################################################################################
@@ -11,15 +10,3 @@ REPOS_TO_CLONE = [
 
 NAME_ORGANIZATION_BITBUCKET = 'sentra-excmapi'
 NAME_ORGANIZATION_GITHUB = 'ogranizacionPrueba123'
-
-'''
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-'''
