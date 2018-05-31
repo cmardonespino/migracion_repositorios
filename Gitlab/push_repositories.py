3eaadc05830888a95c72a27a8c7e21e2cba60de1
Carlos Mardones Pino <excmapi@dom_bci_prod.cl>
2018-05-31 10:57:53 -0400

    Se modifican lineas de codigo

--- Gitlab/push_repositories.py
+++ Gitlab/push_repositories.py
@@ -1,8 +1,7 @@
 # -- coding: utf-8 --
 
 import os, sys
-from variables import USER_NAME
-
+from variables import NAME_ORGANIZATION_BITBUCKET
 # https://developer.atlassian.com/bitbucket/api/2/reference/meta/filtering
 
 carpetas_en_el_directorio = os.listdir("./repositorios")
@@ -16,12 +15,8 @@ path_principal = os.getcwd()
 ID_ORIGEN = 0
 
 for name in carpetas_en_el_directorio:
-	os.chdir("./repositorios/"+name)
-	#os.system("git init")
-	os.system("git remote add origin-"+str(ID_ORIGEN)+" git@bitbucket.org:"+USER_NAME+"/"+name.lower())
-	print("git remote add origin-"+str(ID_ORIGEN)+" git@bitbucket.org:"+USER_NAME+"/"+name.lower())
+	os.chdir(path_principal+"/repositorios/"+name)
+	os.system("git remote add origin-"+str(ID_ORIGEN)+" git@bitbucket.org:"+NAME_ORGANIZATION_BITBUCKET+"/"+name.lower())
 	os.system("git push origin-"+str(ID_ORIGEN)+" --mirror")
 	os.chdir(path_principal)
 	ID_ORIGEN += 1
-
-#os.system('rm -rf repositorios/')
