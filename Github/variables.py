2a96b203cad9b3232e573a8d47ed351b56a8fdbe
cmardonespino <cmardonespino@gmail.com>
2018-06-07 23:13:55 -0400

    Se quita repositorio de muestra

--- Github/variables.py
+++ Github/variables.py
@@ -1,13 +1,12 @@
 # -- coding: utf-8 --
 
 REPOS_TO_CLONE = [
-	'repoPrueba5'
 ]
 
 ################################################################################################
 #Con estas variables se tiene acceso al nombre de todos los repositorios privados de un usuario#
 ################################################################################################
 
-NAME_ORGANIZATION_BITBUCKET = 'sentra-excmapi'
-NAME_ORGANIZATION_GITHUB = 'ogranizacionPrueba123'
+NAME_ORGANIZATION_BITBUCKET = ''
+NAME_ORGANIZATION_GITHUB = ''
 
